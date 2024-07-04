d29c168811672b84c077801a2668f883dea41b84
Onixfff <danilnikonov01@mail.ru>
2024-07-04 15:21:11 +0200

    Перенес класс отдельно

--- Downtime table/Database.cs
+++ Downtime table/Database.cs
@@ -807,16 +807,4 @@ namespace Downtime_table
             }
         }
     }
-
-    public class Recept
-    {
-        public string Name { get; set; }
-        public TimeSpan Time { get; private set; }
-
-        public Recept(string name, TimeSpan timeSpan = default(TimeSpan))
-        {
-            Name = name;
-            Time = timeSpan == default(TimeSpan) ? new TimeSpan(0, 7, 30) : timeSpan;
-        }
-    }
 }
---
+++ Downtime table/Recept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Downtime_table
+{
+    public class Recept
+    {
+        public string Name { get; set; }
+        public TimeSpan Time { get; private set; }
+
+        public Recept(string name, TimeSpan timeSpan = default(TimeSpan))
+        {
+            Name = name;
+            Time = timeSpan == default(TimeSpan) ? new TimeSpan(0, 7, 30) : timeSpan;
+        }
+    }
+}
