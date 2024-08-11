95e6512fdb52c4ae9b1f603ee8a38889aba39fe3
Onixfff <danilnikonov01@mail.ru>
2024-08-11 18:51:58 +0200

    удалил using

--- Downtime table/Form1.cs
+++ Downtime table/Form1.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Downtime_table
